9fd9e6ee1aaf2abd2a3aa700e571ca5f4d80660f
babladey275 <babladey275@gmail.com>
2024-11-30 08:23:51 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-center text-purple-800 font-bold">
+        Vite + React
+      </h1>
+    </>
+  );
+}
+
+export default App;
