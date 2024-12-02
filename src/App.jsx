9d06c405b24b03d717f310174cb0256d4764d4e8
babladey275 <babladey275@gmail.com>
2024-12-02 12:54:00 +0600

    card delete functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import { useLoaderData } from "react-router-dom";
 import CoffeeCard from "./components/CoffeeCard";
+import { useState } from "react";
 
 function App() {
-  const coffees = useLoaderData();
+  const loadedCoffees = useLoaderData();
+  const [coffees, setCoffees] = useState(loadedCoffees);
+
+  const handleDeleteCoffee = (id) => {
+    const remaining = coffees.filter((coffee) => coffee._id !== id);
+    // console.log("Remaining coffees:", remaining);
+    setCoffees(remaining);
+  };
 
   return (
     <>
-      <h1 className="text-4xl text-center text-purple-800 font-bold">
+      <h1 className="text-4xl text-center text-purple-800 font-bold mb-8">
         Number of coffee: {coffees.length}
       </h1>
       <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6">
         {coffees.map((coffee) => (
-          <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>
+          <CoffeeCard
+            key={coffee._id}
+            coffee={coffee}
+            onDelete={handleDeleteCoffee}
+          ></CoffeeCard>
         ))}
       </div>
     </>
--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,7 +1,38 @@
 /* eslint-disable react/prop-types */
 import { FaEdit, FaEye, FaTrash } from "react-icons/fa";
+import Swal from "sweetalert2";
 
-const CoffeeCard = ({ coffee }) => {
+const CoffeeCard = ({ coffee, onDelete }) => {
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        onDelete(id);
+        fetch(`http://localhost:5000/coffee/${coffee._id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deleteCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your coffee has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div className="bg-[#F5F4F1] shadow-lg rounded-lg p-4 flex items-center justify-between space-x-4">
       <div className="flex items-center">
@@ -37,7 +68,10 @@ const CoffeeCard = ({ coffee }) => {
         <button className="flex items-center justify-center w-8 h-8 bg-black rounded-md hover:bg-gray-800">
           <FaEdit className="text-white" />
         </button>
-        <button className="flex items-center justify-center w-8 h-8 bg-red-500 rounded-md hover:bg-red-600">
+        <button
+          onClick={() => handleDelete(coffee._id)}
+          className="flex items-center justify-center w-8 h-8 bg-red-500 rounded-md hover:bg-red-600"
+        >
           <FaTrash className="text-white" />
         </button>
       </div>
